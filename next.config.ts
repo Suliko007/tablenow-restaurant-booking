892e7410a54f2ad8df3a954dd08350c591042790
Suliko007 <nikajalabadze007@gmail.com>
2025-03-19 14:22:20 +0400

    Add sizes property to images config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
+    sizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
   },
   /* config options here */
 };
